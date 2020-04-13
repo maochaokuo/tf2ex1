0f239847bb18cc8ae0070e0c6538e0469ae22dfd
Thomas Mao <tm1813@gmail.com>
2020-04-13 22:46:53 +0800

    add trainEvaluate.py

--- trainEvaluate.py
+++ trainEvaluate.py
@@ -14,3 +14,30 @@ loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 batch_size = 64
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+
+
+
+# Get model
+inputs = keras.Input(shape=(784,), name='digits')
+x = layers.Dense(64, activation='relu', name='dense_1')(inputs)
+x = layers.Dense(64, activation='relu', name='dense_2')(x)
+outputs = layers.Dense(10, name='predictions')(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+# Instantiate an optimizer to train the model.
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+# Instantiate a loss function.
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+# Prepare the metrics.
+train_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+val_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+
+# Prepare the training dataset.
+batch_size = 64
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+
+# Prepare the validation dataset.
+val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+val_dataset = val_dataset.batch(64)
